57e41efd4c600230d245442aca1af29e81c64403
Denis Ivanov <dnb8866@gmail.com>
2024-09-22 16:25:31 +0700

    progress planner -> update class Day

--- planner/entities.py
+++ planner/entities.py
@@ -1,17 +1,22 @@
 from datetime import time, timedelta, datetime
-from pprint import pprint
 
-import config as cfg
-from utils.assist import frange
 from utils.constants import PLANNER_SCHEDULE
 from utils.models_orm import Slot, Event
 
 
 class Day:
     def __init__(self, slots: list[Slot], schedule: dict = PLANNER_SCHEDULE):
-        self.slots = slots
         self._schedule = schedule
-        self.create_daily_schedule()
+        self.create_daily_schedule(slots)
+
+    @staticmethod
+    def _check_slots(slots: list[Slot]):
+        dates = set()
+        for slot in slots:
+            dates.add(slot.start_date)
+            if len(dates) > 1:
+                # logger.error()
+                raise ValueError('Slots contain more than one date')
 
     def _delete_used_slot(self, slot: Slot):
         dt = datetime.combine(datetime.today(), slot.start_time)
@@ -20,8 +25,8 @@ class Day:
             if slot.start_time < time_from_schedule < time_end:
                 self._schedule.pop(time_from_schedule)
 
-    def create_daily_schedule(self) -> None:
-        for slot in self.slots:
+    def create_daily_schedule(self, slots: list[Slot]) -> None:
+        for slot in slots:
             self._delete_used_slot(slot)
             self._schedule[slot.start_time] = slot
 
